e70c320cb45ba0ffaede3dbab155124ebb0ecc87
farhan-murtaza <farhanmurtza03@gmail.com>
2024-08-06 16:13:46 +0500

    Add Authentication to this app using spotify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
+	import type { LayoutData } from './$types';
+	import LogoutButton from '$lib/components/LogoutButton.svelte';
+	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
 
+{#if user}
+	<p>Hello {user.display_name}</p>
+	<LogoutButton />
+{/if}
 <slot />
