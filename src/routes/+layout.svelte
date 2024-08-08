d929e0b13a6044ad2673fbc75e70d68c139c4e38
farhan-murtaza <farhanmurtza03@gmail.com>
2024-08-08 19:56:58 +0500

    Working on styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,37 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
-	import LogoutButton from '$lib/components/LogoutButton.svelte';
+	import Navigation from '$components/Navigation.svelte';
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
 
-{#if user}
-	<p>Hello {user.display_name}</p>
-	<LogoutButton />
-{/if}
-<slot />
+<div id="main">
+	{#if user}
+		<div id="sidebar">
+			<Navigation destop={true} />
+		</div>
+	{/if}
+	<div id="content">
+		<main id="main-content">
+			<slot />
+		</main>
+	</div>
+</div>
+
+<style lang="scss">
+	#main {
+		display: flex;
+
+		#content {
+			flex: 1;
+			#main-content {
+				padding: 30px 15px 60px;
+				@include breakpoint.up('md') {
+					padding: 30px 30px 60px;
+				}
+			}
+		}
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: sveltePreprocess({
 		scss: {
-			prependData: '@use "../styles/functions"; @use "@unsass/breakpoint";'
+			prependData: '@use "src/styles/functions"; @use "@unsass/breakpoint";'
 		}
 	}),
 
@@ -15,7 +15,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			$assets: 'src/assets'
+		}
 	}
 };
 
